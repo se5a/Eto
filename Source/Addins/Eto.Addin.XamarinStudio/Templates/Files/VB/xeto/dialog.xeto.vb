56b94cf5b544dec55e4563249f63bf4c0133be78
se5a <jon.se5a@gmail.com>
2017-06-06 16:07:53 +0200

    Added the new dialog and form files to the commit this time. Actual.

---
+++ Source/Addins/Eto.Addin.XamarinStudio/Templates/Files/VB/xeto/dialog.xeto.vb
@@ -0,0 +1,14 @@
+﻿Imports Eto.Forms
+Imports Eto.Drawing
+Imports Eto.Serialization.Xaml
+
+Public Class ${EscapedIdentifier}
+	Inherits Dialog
+
+	Public Sub New ()
+
+		XamlReader.Load (Me)
+
+	End Sub
+
+End Class
